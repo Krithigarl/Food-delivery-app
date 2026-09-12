702b7e84b36bcc2408a53610ac98ed5a09b44b3b
KrithigaRL <rlkirthiga@gmail.com>
2026-09-12 10:14:58 +0530

    Fix MongoDB connection for Vercel

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -6,10 +6,11 @@ dotenv.config();
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI as string);
+    await mongoose.connect(process.env.MONGODB_URI as string);
     console.log('MongoDB connected');
   } catch (error) {
     console.error(error);
-    process.exit(1);
+    // process.exit(1);
+    throw error;
   }
 };
