ba9a8d2b462b8eed6e16f5810eaaa6bef3514bb4
KrithigaRL <rlkirthiga@gmail.com>
2026-09-12 09:48:41 +0530

    update fronted

--- Frontend/Frontend/src/Dashboard/Addfoodform.jsx
+++ Frontend/Frontend/src/Dashboard/Addfoodform.jsx
@@ -46,7 +46,7 @@ const AddFood = () => {
 
     axios
       .post(
-        "http://localhost:5000/api/admin/add-food",
+        import.meta.env.VITE_API_URL + "/api/admin/add-food",
         form,
         {
           headers: {
