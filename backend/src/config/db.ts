15413dc9c69ffb6cf28c9b8b56fa469193c229d3
KrithigaRL <rlkirthiga@gmail.com>
2026-09-12 10:18:26 +0530

    up

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -6,7 +6,7 @@ dotenv.config();
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI as string);
+    await mongoose.connect(process.env.MONGO_URI as string);
     console.log('MongoDB connected');
   } catch (error) {
     console.error(error);
